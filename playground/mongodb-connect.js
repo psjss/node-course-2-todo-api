b4dbcbb58058adbb959c0de38f1e824f314a50da
Prateek Singhania <prateekcool2001@gmail.com>
2017-11-07 00:46:58 +0530

    Add delete script

--- playground/mongodb-connect.js
+++ playground/mongodb-connect.js
@@ -21,18 +21,18 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
     //    console.log(JSON.stringify(result.ops, undefined, 2));
     // });
 
-    // db.collection('Users').insertOne({
-    //         name: 'Prateek Singhania',
-    //         age: 25,
-    //         location: 'Noida'
-    // }, (err, result) => {
-    //     if(err) {
-    //         return console.log('unable to insert todo', err);
-    //     }
-    //
-    //     //console.log(JSON.stringify(result.ops, undefined, 2));
-    //       console.log(result.ops[0]._id.getTimestamp());
-    // });
+    db.collection('Users').insertOne({
+            name: 'Prats',
+            age: 25,
+            location: 'Noida'
+    }, (err, result) => {
+        if(err) {
+            return console.log('unable to insert todo', err);
+        }
+
+        //console.log(JSON.stringify(result.ops, undefined, 2));
+          console.log(result.ops[0]._id.getTimestamp());
+    });
 
     db.close();
 });
