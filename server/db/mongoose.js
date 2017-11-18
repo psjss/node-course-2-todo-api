d4975a97e81239aa981fcff63046746c1f71e3ab
Prateek Singhania <prateekcool2001@gmail.com>
2017-11-18 22:37:27 +0530

    test

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,12 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
+console.log(process.env.MONGODB_URI)
 
-let db = {
-    localhost: 'mongodb://localhost:27017/TodoApp',
-    mlab: 'mongodb://admin:admin@ds163745.mlab.com:63745/todoapp
-};
-
-mongoose.connect(db.localhost || db.mlab);
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
 
 module.exports = {mongoose};
